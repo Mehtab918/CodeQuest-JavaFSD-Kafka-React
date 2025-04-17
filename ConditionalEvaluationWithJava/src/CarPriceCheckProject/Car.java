5cf627d423450bb04fcb66e189ccf729a1db32bd
Mehtab918 <khanmehtab4@gmail.com>
2025-04-17 17:07:13 +0530

    first commit

---
+++ ConditionalEvaluationWithJava/src/CarPriceCheckProject/Car.java
@@ -0,0 +1,23 @@
+package CarPriceCheckProject;
+
+import java.util.Scanner;
+public class Car {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Enter the car price for checking:");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int priceOfCar = sc.nextInt();
+		
+		System.out.println("Price of the car :" + priceOfCar);
+		
+		if(priceOfCar <=10000)
+		System.out.println("price of the car is affordable");
+		
+		System.out.println("completed");
+	}
+
+}
